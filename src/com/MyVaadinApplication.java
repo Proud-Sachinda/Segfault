c20097e855b1fd14a6bd9fd8dbc56f146beff518
Ntsiki-Nonjo <815108@students.wits.ac.za>
2019-02-24 17:32:45 +0200

    local

--- src/com/MyVaadinApplication.java
+++ src/com/MyVaadinApplication.java
@@ -3,7 +3,7 @@ package com;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.ui.*;
 
-public class QuestionActivity extends UI {
+public class MyVaadinApplication extends UI {
     @Override
     public void init(VaadinRequest request) {
         VerticalLayout layout = new VerticalLayout();
