28c17a50483ebcdbafe7eb7b3aab9b076bf5d968
Proud-Sachinda <1340076@students.wits.ac.za>
2019-02-25 17:35:25 +0200

    travis

--- src/test/java/com/MyUITest.java
+++ src/test/java/com/MyUITest.java
@@ -3,14 +3,36 @@ package com;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
+import org.junit.*;
+//import static net.sourceforge.jwebunit.junit.JWebUnit.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+/*
 class MyUITest {
 
     @Test
     void multiply() {
         MyUI ui = new MyUI();
 
+        Assert.assertEquals(ui.multiply(5, 2), 10);
+    }
+}
+
+*/
+
+//http://localhost:8000/Segfault_war_exploded/       :::::This our welcome page address
+
+public class MyUITest{
+    @Before
+    public  void prepare() {
+      //  setBaseUrl("http://localhost:8080/test");
+
+    }
+
+    @Test
+    public void testBasic(){
+        MyUI ui = new MyUI();
         Assert.assertEquals(ui.multiply(5, 2), 10);
     }
 }
