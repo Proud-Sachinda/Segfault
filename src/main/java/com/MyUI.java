b598d950f4ebf3eb65f9d3b62d251a27edd3fb3b
Proud-Sachinda <30469646+Proud-Sachinda@users.noreply.github.com>
2019-04-02 22:58:23 +0200

    Update MyUI.java

--- src/main/java/com/MyUI.java
+++ src/main/java/com/MyUI.java
@@ -37,8 +37,8 @@ public class MyUI extends UI {
         Connection connection = null;
         try {
             connection = DriverManager
-                    .getConnection("jdbc:postgresql://localhost:5432/qbank",
-                            "postgres", "@6125@psN");
+                    .getConnection("jdbc:postgresql://localhost:5432/postgres",
+                            "postgres", "postgres");
 
         } catch (SQLException e) {
             e.printStackTrace();
