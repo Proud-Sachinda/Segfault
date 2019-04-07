dfe48620c088f3578a54aeb8656edad31b5cc53e
Proud-Sachinda <1340076@students.wits.ac.za>
2019-04-07 16:50:24 +0200

    surefire reports from junit

---
+++ src/Test/com/Server/CourseServerTest.java
@@ -0,0 +1,34 @@
+package com.Server;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CourseServerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void showCourse() {
+    }
+
+    @Test
+    void postCourse() {
+    }
+
+    @Test
+    void getCourse() {
+    }
+}
--- src/Test/com/Server/QuestionServerTest.java
+++ src/Test/com/Server/QuestionServerTest.java
@@ -3,6 +3,7 @@ package com.Server;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+
 import java.sql.Connection;
 
 import static org.junit.jupiter.api.Assertions.*;
