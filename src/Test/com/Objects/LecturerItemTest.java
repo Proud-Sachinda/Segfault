d18bf0632075a770304bb723b194781def7ce2d0
Proud-Sachinda <1340076@students.wits.ac.za>
2019-05-21 01:30:26 +0200

    lectureItem

---
+++ src/Test/com/Objects/LecturerItemTest.java
@@ -0,0 +1,54 @@
+package com.Objects;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LecturerItemTest {
+
+    LecturerItem lecturerItem = new LecturerItem("1");
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getLecturerId() {
+
+        lecturerItem.setLecturerId("5");
+        assertEquals("5",lecturerItem.getLecturerId());
+    }
+
+    @Test
+    void setLecturerId() {
+        lecturerItem = new LecturerItem("5");
+        assertEquals("5",lecturerItem.getLecturerId());
+    }
+
+    @Test
+    void getLecturerFname() {
+
+        lecturerItem.setLecturerFname("Terence");
+        assertEquals("Terence",lecturerItem.getLecturerFname());
+    }
+
+    @Test
+    void setLecturerFname() {
+
+        lecturerItem.setLecturerFname("Terence");
+        assertEquals("Terence",lecturerItem.getLecturerFname());
+    }
+
+    @Test
+    void getLecturerLname() {
+        lecturerItem.setLecturerLname("Van Zyl");
+        assertEquals("Van Zyl",lecturerItem.getLecturerLname());
+    }
+
+    @Test
+    void setLecturerLname() {
+        lecturerItem.setLecturerLname("Van Zyl");
+        assertEquals("Van Zyl",lecturerItem.getLecturerLname());
+    }
+}
