cd8bbcbe3170697567066f1c70928125737c9750
LizoRadebe <1417773@students.wits.ac.za>
2019-05-23 23:33:50 +0200

    again.

--- src/main/java/com/Client/SignUpView.java
+++ src/main/java/com/Client/SignUpView.java
@@ -87,7 +87,7 @@ public class SignUpView extends VerticalLayout implements View {
                         navigator.navigateTo("editor");
                     }
                     else {
-                    Notification.show("Requied Field", Notification.Type.ERROR_MESSAGE);
+                    Notification.show(" Field", Notification.Type.ERROR_MESSAGE);
                 }
 
 
