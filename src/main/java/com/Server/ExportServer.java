4c0b7846b5529bcd229dccd05f386be27ed275ef
tanki97 <1107943@students.wits.ac.za>
2019-05-25 09:19:42 +0200

    dir

--- src/main/java/com/Server/ExportServer.java
+++ src/main/java/com/Server/ExportServer.java
@@ -92,7 +92,7 @@ public class ExportServer {
         ts = new TestServer(connection);
         ti = ts.getTestItemById(tId);
         String testname = ti.getTestDraftName();
-        File f = new File("C:\\+"+testname);
+        File f = new File("C:\\"+testname);
         try{
             if(f.mkdir()) {
                 System.out.println("Directory Created");
@@ -102,18 +102,18 @@ public class ExportServer {
                 Path sourceDirectory3 = Paths.get("C:\\Users\\User\\IdeaProjects\\Segfault\\Wits packages\\wits_flowchart.sty");
                 Path sourceDirectory4 = Paths.get("C:\\Users\\User\\IdeaProjects\\Segfault\\Wits packages\\wits_pseudocode.sty");
 
-                Path targetDirectory = Paths.get("C:\\Users\\User\\Desktop\\aaa\\wits_code.sty");
-                Path targetDirectory1 = Paths.get("C:\\Users\\User\\Desktop\\aaa\\wits_exam.sty");
-                Path targetDirectory2 = Paths.get("C:\\Users\\User\\Desktop\\aaa\\wits_question.sty");
-                Path targetDirectory3 = Paths.get("C:\\Users\\User\\Desktop\\aaa\\wits_flowchart.sty");
-                Path targetDirectory4 = Paths.get("C:\\Users\\User\\Desktop\\aaa\\wits_pseudocode.sty");
+                Path targetDirectory = Paths.get("C:\\"+testname+"\\wits_code.sty");
+                Path targetDirectory1 = Paths.get("C:\\"+testname+"\\wits_exam.sty");
+                Path targetDirectory2 = Paths.get("C:\\"+testname+"\\wits_question.sty");
+                Path targetDirectory3 = Paths.get("C:\\"+testname+"\\wits_flowchart.sty");
+                Path targetDirectory4 = Paths.get("C:\\"+testname+"\\wits_pseudocode.sty");
 
                 Files.copy(sourceDirectory, targetDirectory);
                 Files.copy(sourceDirectory1, targetDirectory1);
                 Files.copy(sourceDirectory2, targetDirectory2);
                 Files.copy(sourceDirectory3, targetDirectory3);
                 Files.copy(sourceDirectory4, targetDirectory4);
-                BufferedWriter writer = new BufferedWriter(new FileWriter("C:\\Users\\User\\Desktop\\aaa\\hi.tex"));
+                BufferedWriter writer = new BufferedWriter(new FileWriter("C:\\"+testname+"\\"+testname+".tex"));
                 String str = GenerateLatex(ex);
                 int questionNoCount = getTestITemQuestionCount(tId);
                 for(int y=1;y<questionNoCount+1;y++){
