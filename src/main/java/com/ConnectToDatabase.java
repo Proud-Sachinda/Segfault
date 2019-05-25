0233f7d2fae16db510e471824e057544cd05237b
Shivesh <shivmaharaj16@live.co.za>
2019-05-25 11:14:56 +0200

    heroku

--- src/main/java/com/ConnectToDatabase.java
+++ src/main/java/com/ConnectToDatabase.java
@@ -1,10 +1,16 @@
 package com;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectToDatabase {
+
+
+
+
 
     public static Connection getConnection() {
 
@@ -13,20 +19,20 @@ public class ConnectToDatabase {
         // first attempt
         try {
             connection = DriverManager
-                    .getConnection("jdbc:postgresql://ec2-54-75-245-196.eu-west-1.compute.amazonaws.com:5432/d78v0ec8r1pino?sslmode=require","ockwjgxlnqiboa" ,"f0d9966767f07b2031f1d942c128cf3e16e2fd93d575c640f735bca0ac8b3681");
+                    .getConnection("jdbc:postgresql://ec2-54-75-245-196.eu-west-1.compute.amazonaws.com:5432/d78v0ec8r1pino?sslmode=require&user=ockwjgxlnqiboa&password=f0d9966767f07b2031f1d942c128cf3e16e2fd93d575c640f735bca0ac8b3681");
 
         } catch (SQLException e) {
 
             // second attempt
             try {
                 connection = DriverManager
-                        .getConnection("jdbc:postgresql://ec2-54-75-245-196.eu-west-1.compute.amazonaws.com:5432/d78v0ec8r1pino?sslmode=require","ockwjgxlnqiboa" ,"f0d9966767f07b2031f1d942c128cf3e16e2fd93d575c640f735bca0ac8b3681");
+                        .getConnection("jdbc:postgresql://ec2-54-75-245-196.eu-west-1.compute.amazonaws.com:5432/d78v0ec8r1pino?sslmode=require&user=ockwjgxlnqiboa&password=f0d9966767f07b2031f1d942c128cf3e16e2fd93d575c640f735bca0ac8b3681");
             } catch (SQLException ex) {
 
                 // third attempt
                 try {
                     connection = DriverManager
-                            .getConnection("jdbc:postgresql://ec2-54-75-245-196.eu-west-1.compute.amazonaws.com:5432/d78v0ec8r1pino?sslmode=require","ockwjgxlnqiboa" ,"f0d9966767f07b2031f1d942c128cf3e16e2fd93d575c640f735bca0ac8b3681");
+                            .getConnection("jdbc:postgresql://ec2-54-75-245-196.eu-west-1.compute.amazonaws.com:5432/d78v0ec8r1pino?sslmode=require&user=ockwjgxlnqiboa&password=f0d9966767f07b2031f1d942c128cf3e16e2fd93d575c640f735bca0ac8b3681");
                 } catch (SQLException exc) {
                     exc.printStackTrace();
                 }
