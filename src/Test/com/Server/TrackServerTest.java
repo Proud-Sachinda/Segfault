59010ec51781c6777560b0af3eb2b010b0e32141
tanki97 <1107943@students.wits.ac.za>
2019-05-29 11:21:08 +0200

    track test

--- src/Test/com/Server/TrackServerTest.java
+++ src/Test/com/Server/TrackServerTest.java
@@ -1,5 +1,6 @@
 package com.Server;
 
+import com.Objects.TrackItem;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
@@ -11,7 +12,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Matchers.anyString;
 
 class TrackServerTest {
@@ -28,18 +28,26 @@ class TrackServerTest {
 
     @Mock
     TrackServer ts;
+    TrackItem ti;
 
     @BeforeEach
     void setUp() throws  Exception{
         MockitoAnnotations.initMocks(this);
+       /* ti.setTrackId(1);
+        ti.setQuestionId(1);
+        ti.setTestId(1);
+        ti.setQuestionNumber(1);
+        ti.setTrackOrder(1);*/
         Mockito.when(connection.createStatement()).thenReturn(statement);
         Mockito.when(statement.executeUpdate(anyString())).thenReturn(1);
+
     }
 
     @Test
     void postToTrackTable() throws Exception{
 
         ts = new TrackServer(connection);
+        ti = ts.postToTrackTable(1,1,1,1);
         Mockito.verify(connection, Mockito.times(1)).createStatement();
         Mockito.verify(statement, Mockito.times(1)).executeUpdate(anyString());
     }
